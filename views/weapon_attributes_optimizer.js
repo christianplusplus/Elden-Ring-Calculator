694531c9a3a91be6f5b43ec758f52fa26a72ffe8
christianplusplus <christian.wendlandt.cs@gmail.com>
2022-04-19 10:45:58 -0500

    Added Flexbox

--- views/weapon_attributes_optimizer.js
+++ views/weapon_attributes_optimizer.js
@@ -32,57 +32,103 @@ var WeaponAttributesOptimizer = {
         }
     },
     template:`
-<select name="enemy" v-model="args.enemy">
-    <option v-for="boss in args.bosses" :value="boss">
-        {{ boss.Name }}
-    </option>
-</select>
-<label for="enemy">:Enemy</label>
-<br>
-<select name="weaponTypes" v-model="args.weapon_types_selected" multiple>
-    <option v-for="weapon_type in args.weapon_types">{{ weapon_type }}</option>
-</select>
-<label for="weaponTypes">:Weapon Types</label>
-<br>
-<select name="affinities" v-model="args.affinities_selected" multiple>
-    <option v-for="affinity in args.affinities">{{ affinity }}</option>
-</select>
-<label for="affinities">:Affinities</label>
-<br>
-<input type="checkbox" name="isDualWieldable" v-model="args.is_dual_wieldable" :true-value=true :false-value=false>
-<label for="isDualWieldable">:Is Dual Wieldable</label>
-<br>
-<input type="checkbox" name="isTwoHanding" v-model="args.is_two_handing" :true-value=true :false-value=false>
-<label for="isTwoHanding">:Is Two Handing</label>
-<br>
-<input type="checkbox" name="meetsAttributeRequirements" v-model="must_have_required_attributes" :true-value=true :false-value=false>
-<label for="meetsAttributeRequirements">:Meets Attribute Requirements</label>
-<br>
-<select v-model="args.clazz">
-    <option v-for="[class_name, clazz] in Object.entries(args.class_stats)" :value="clazz">
-        {{ class_name[0].toUpperCase() + class_name.slice(1) }}
-    </option>
-</select>
-<button type="button" @click="$emit('load_class')">Load Class</button>
-<br>
-<input type="number" v-model.number="args.str" min="1" max="99">
-<label for="str">:Strength</label>
-<br>
-<input type="number" v-model.number="args.dex" min="1" max="99">
-<label for="dex">:Dexterity</label>
-<br>
-<input type="number" v-model.number="args.int" min="1" max="99">
-<label for="int">:Intelligence</label>
-<br>
-<input type="number" v-model.number="args.fai" min="1" max="99">
-<label for="fai">:Faith</label>
-<br>
-<input type="number" v-model.number="args.arc" min="1" max="99">
-<label for="arc">:Arcane</label>
-<br>
-<input type="number" v-model.number="args.floatingPoints" min="0" max="445">
-<label for="floatingPoints">:Floating Points</label>
-<br>
+<div>
+    <div>
+        <label for="weaponTypes">Weapon Types</label>
+        <br>
+        <select size="14" name="weaponTypes" v-model="args.weapon_types_selected" multiple>
+            <option v-for="weapon_type in args.weapon_types">{{ weapon_type }}</option>
+        </select>
+    </div>
+    <div>
+        <label for="affinities">Affinities</label>
+        <br>
+        <select class="selectNoScroll" size="14" name="affinities" v-model="args.affinities_selected" multiple>
+            <option v-for="affinity in args.affinities">{{ affinity }}</option>
+        </select>
+    </div>
+    <div>
+        <input type="checkbox" name="isDualWieldable" v-model="args.is_dual_wieldable" :true-value=true :false-value=false>
+        <label for="isDualWieldable"> Dual Wieldable</label>
+        <br>
+        <input type="checkbox" name="isTwoHanding" v-model="args.is_two_handing" :true-value=true :false-value=false>
+        <label for="isTwoHanding"> Two Handing</label>
+        <br>
+        <input type="checkbox" name="meetsAttributeRequirements" v-model="must_have_required_attributes" :true-value=true :false-value=false>
+        <label for="meetsAttributeRequirements"> Required Attributes</label>
+    </div>
+    <div>
+        <button type="button" @click="$emit('load_class')">Load Class</button>
+        <br>
+        <select v-model="args.clazz">
+            <option v-for="[class_name, clazz] in Object.entries(args.class_stats)" :value="clazz">
+                {{ class_name[0].toUpperCase() + class_name.slice(1) }}
+            </option>
+        </select>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.str" min="1" max="99">
+                </td>
+                <td>
+                    <label for="str">STR</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.dex" min="1" max="99">
+                </td>
+                <td>
+                    <label for="dex">DEX</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.int" min="1" max="99">
+                </td>
+                <td>
+                    <label for="int">INT</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.fai" min="1" max="99">
+                </td>
+                <td>
+                    <label for="fai">FAI</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.arc" min="1" max="99">
+                </td>
+                <td>
+                    <label for="arc">ARC</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="number" v-model.number="args.floatingPoints" min="0" max="445">
+                </td>
+                <td>
+                    <label for="floatingPoints">FLOAT</label>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div>
+    <div>
+        <label for="enemy">Enemy </label>
+        <select name="enemy" v-model="args.enemy">
+            <option v-for="boss in args.bosses" :value="boss">
+                {{ boss.Name }}
+            </option>
+        </select>
+    </div>
+</div>
 <button @click="calculate">Calculate!</button>
 `,
 };
