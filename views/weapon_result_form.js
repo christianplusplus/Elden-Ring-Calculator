9a3b4d3893820403ee7899a0b0b45820e5a8823a
christianplusplus <christian.wendlandt.cs@gmail.com>
2022-04-22 17:03:50 -0500

    style needs more work

--- views/weapon_result_form.js
+++ views/weapon_result_form.js
@@ -7,12 +7,12 @@ var weaponResultForm = {
     <div class="t0">
         <div style="font-size:20px;color:gold;">{{ result.weapon.name }}</div>
         <div class='stat_box'>
-            <div><div>{{ result.weapon.weapon_type }}</div></div>
-            <div><div>{{ result.weapon.physical_damage_types.join('/') }}</div></div>
-            <div><div>{{ result.weapon.affinity }}</div></div>
-            <div><div>{{ 'Is ' + (result.weapon.dual_wieldable ? '' : 'Not') + ' Dual Wieldable' }}</div></div>
-            <div><div>Weight</div><div>{{ result.weapon.weight }}</div></div>
-            <div><div>From</div><a style="color:cyan;" :href="'https://eldenring.wiki.fextralife.com/'+result.weapon.base_weapon_name.replaceAll(' ', '+')">{{ result.weapon.base_weapon_name }}</a></div>
+            <div class="filler"><div>{{ result.weapon.weapon_type }}</div></div>
+            <div class="filler"><div>{{ result.weapon.physical_damage_types.join('/') }}</div></div>
+            <div class="filler"><div>{{ result.weapon.affinity }}</div></div>
+            <div class="filler"><div>{{ 'Is ' + (result.weapon.dual_wieldable ? '' : 'Not') + ' Dual Wieldable' }}</div></div>
+            <div class="filler"><div>Weight</div><div>{{ result.weapon.weight }}</div></div>
+            <div class="filler"><div>From</div><a style="color:cyan;" :href="'https://eldenring.wiki.fextralife.com/'+result.weapon.base_weapon_name.replaceAll(' ', '+')">{{ result.weapon.base_weapon_name }}</a></div>
         </div>
     </div>
     <div class="p0 damage_result">
@@ -21,7 +21,7 @@ var weaponResultForm = {
     <div class="t1">
         <div>Attack Power</div>
         <div class='stat_box'>
-            <div>
+            <div class="filler">
                 <div>Physical</div>
                 <div>{{ result.weapon.base_attack_power.physical }}</div>
                 <div>{{ result.weapon.bonus_attack_power.physical ? '+' : '' }}</div>
@@ -29,7 +29,7 @@ var weaponResultForm = {
                 <div>=</div>
                 <div>{{ result.weapon.attack_power.physical }}</div>
             </div>
-            <div>
+            <div class="filler">
                 <div>Magic</div>
                 <div>{{ result.weapon.base_attack_power.magic }}</div>
                 <div>{{ result.weapon.bonus_attack_power.magic ? '+' : '' }}</div>
@@ -37,7 +37,7 @@ var weaponResultForm = {
                 <div>=</div>
                 <div>{{ result.weapon.attack_power.magic }}</div>
             </div>
-            <div>
+            <div class="filler">
                 <div>Fire</div>
                 <div>{{ result.weapon.base_attack_power.fire }}</div>
                 <div>{{ result.weapon.bonus_attack_power.fire ? '+' : '' }}</div>
@@ -45,7 +45,7 @@ var weaponResultForm = {
                 <div>=</div>
                 <div>{{ result.weapon.attack_power.fire }}</div>
             </div>
-            <div>
+            <div class="filler">
                 <div>Lightning</div>
                 <div>{{ result.weapon.base_attack_power.lightning }}</div>
                 <div>{{ result.weapon.bonus_attack_power.lightning ? '+' : '' }}</div>
@@ -53,7 +53,7 @@ var weaponResultForm = {
                 <div>=</div>
                 <div>{{ result.weapon.attack_power.lightning }}</div>
             </div>
-            <div>
+            <div class="filler">
                 <div>Holy</div>
                 <div>{{ result.weapon.base_attack_power.holy }}</div>
                 <div>{{ result.weapon.bonus_attack_power.holy ? '+' : '' }}</div>
@@ -66,25 +66,25 @@ var weaponResultForm = {
     <div class="t2">
         <div>Attribute Scaling</div>
         <div class='stat_box'>
-            <div><div>Str</div><div>{{ result.weapon.str_scaling_grade }}</div><div></div><div></div><div>Dex</div><div>{{ result.weapon.dex_scaling_grade }}</div></div>
-            <div><div>Int</div><div>{{ result.weapon.int_scaling_grade }}</div><div></div><div></div><div>Fai</div><div>{{ result.weapon.fai_scaling_grade }}</div></div>
-            <div><div>Arc</div><div>{{ result.weapon.arc_scaling_grade }}</div><div></div><div></div><div></div><div></div></div>
+            <div class="filler"><div>Str</div><div>{{ result.weapon.str_scaling_grade }}</div><div></div><div></div><div>Dex</div><div>{{ result.weapon.dex_scaling_grade }}</div></div>
+            <div class="filler"><div>Int</div><div>{{ result.weapon.int_scaling_grade }}</div><div></div><div></div><div>Fai</div><div>{{ result.weapon.fai_scaling_grade }}</div></div>
+            <div class="filler"><div>Arc</div><div>{{ result.weapon.arc_scaling_grade }}</div><div></div><div></div><div></div><div></div></div>
         </div>
     </div>
     <div class="t3">
         <div>Attributes Required</div>
         <div class='stat_box'>
-            <div><div>Str</div><div>{{ result.weapon.required_str }}</div><div></div><div></div><div>Dex</div><div>{{ result.weapon.required_dex }}</div></div>
-            <div><div>Int</div><div>{{ result.weapon.required_int }}</div><div></div><div></div><div>Fai</div><div>{{ result.weapon.required_fai }}</div></div>
-            <div><div>Arc</div><div>{{ result.weapon.required_arc }}</div><div></div><div></div><div></div><div></div></div>
+            <div class="filler"><div>Str</div><div>{{ result.weapon.required_str }}</div><div></div><div></div><div>Dex</div><div>{{ result.weapon.required_dex }}</div></div>
+            <div class="filler"><div>Int</div><div>{{ result.weapon.required_int }}</div><div></div><div></div><div>Fai</div><div>{{ result.weapon.required_fai }}</div></div>
+            <div class="filler"><div>Arc</div><div>{{ result.weapon.required_arc }}</div><div></div><div></div><div></div><div></div></div>
         </div>
     </div>
     <div class="t4">
         <div>Passive Effects</div>
         <div class='stat_box'>
-            <div><div>Poison</div><div>{{ result.weapon.poison || '-' }}</div><div></div><div></div><div>Scarlet&nbsp;Rot</div><div>{{ result.weapon.scarlet_rot || '-' }}</div></div>
-            <div><div>Bleed</div><div>{{ result.weapon.bleed || '-' }}</div><div></div><div></div><div>Frostbite</div><div>{{ result.weapon.frostbite || '-' }}</div></div>
-            <div><div>Sleep</div><div>{{ result.weapon.sleep || '-' }}</div><div></div><div></div><div>Madness</div><div>{{ result.weapon.madness || '-' }}</div></div>
+            <div class="filler"><div>Poison</div><div>{{ result.weapon.poison || '-' }}</div><div></div><div></div><div>Scarlet&nbsp;Rot</div><div>{{ result.weapon.scarlet_rot || '-' }}</div></div>
+            <div class="filler"><div>Bleed</div><div>{{ result.weapon.bleed || '-' }}</div><div></div><div></div><div>Frostbite</div><div>{{ result.weapon.frostbite || '-' }}</div></div>
+            <div class="filler"><div>Sleep</div><div>{{ result.weapon.sleep || '-' }}</div><div></div><div></div><div>Madness</div><div>{{ result.weapon.madness || '-' }}</div></div>
         </div>
     </div>
 </div>
