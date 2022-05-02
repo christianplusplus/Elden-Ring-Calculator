fbb12e9d9b3953d0f2e501595c7264e8752e4995
christianplusplus <christian.wendlandt.cs@gmail.com>
2022-05-02 13:51:47 -0500

    Randomized Progress Bar

--- views/progress_bar.js
+++ views/progress_bar.js
@@ -2,17 +2,34 @@ var progressBar = {
     props: {
         progress: Number,
     },
-    methods: {
+    data() {
+        return {
+            srcs: [
+                "assets/godricks-rune.png",
+                "assets/unborn-rune.png",
+                "assets/radahns-rune.png",
+                "assets/morgotts-rune.png",
+                "assets/rykards-rune.png",
+                "assets/mohgs-rune.png",
+                "assets/malenias-rune.png",
+            ],
+        };
+    },
+    mounted() {
+        for (var i = this.srcs.length - 1; i > 0; i--) {
+            var rand = Math.floor(Math.random() * (i + 1));
+            [this.srcs[i], this.srcs[rand]] = [this.srcs[rand], this.srcs[i]]
+        }
     },
     template:`
 <div class="runes">
-    <img :style="{opacity:Math.min(Math.max((progress*7-0),0),1)}" src="assets/godricks-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-1),0),1)}" src="assets/unborn-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-2),0),1)}" src="assets/radahns-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-3),0),1)}" src="assets/morgotts-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-4),0),1)}" src="assets/rykards-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-5),0),1)}" src="assets/mohgs-rune.png"/>
-    <img :style="{opacity:Math.min(Math.max((progress*7-6),0),1)}" src="assets/malenias-rune.png"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-0),0),1)}" :src="srcs[0]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-1),0),1)}" :src="srcs[1]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-2),0),1)}" :src="srcs[2]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-3),0),1)}" :src="srcs[3]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-4),0),1)}" :src="srcs[4]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-5),0),1)}" :src="srcs[5]"/>
+    <img :style="{opacity:Math.min(Math.max((progress*7-6),0),1)}" :src="srcs[6]"/>
 </div>
 `,
 };
