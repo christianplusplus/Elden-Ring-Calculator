9d88bdfd0b2df2adc0fdcb6e587c11c3b0e79717
christianplusplus <48064080+christianplusplus@users.noreply.github.com>
2022-05-09 20:34:00 -0500

    locking

--- src/compile.bat
+++ src/compile.bat
@@ -1,4 +1,4 @@
 java -jar compiler.jar --js_output_file script.js *.js
-call javascript-obfuscator script.js -o ../script.js --self-defending true --log true --domain-lock 'gideonstruedamage.com'
+call javascript-obfuscator script.js -o ../script.js --self-defending true --log true --domain-lock gideonstruedamage.com
 del script.js
 pause
