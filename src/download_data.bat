6cdc30ebedf7d6e264f8531ee613f36173e81863
christianplusplus <48064080+christianplusplus@users.noreply.github.com>
2022-07-09 18:48:33 -0500

    built curl script

---
+++ src/download_data.bat
@@ -0,0 +1,14 @@
+curl -o ..\src_data\weapon_attribute_attack_power_map.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=1749106382&format=csv"
+curl -o ..\src_data\weapon_base_attack_power.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=724804355&format=csv"
+curl -o ..\src_data\weapon_attribute_scaling.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=670105415&format=csv"
+curl -o ..\src_data\weapon_passive.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=626456404&format=csv"
+curl -o ..\src_data\weapon_damage_graph.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=1714239924&format=csv"
+curl -o ..\src_data\weapons.csv -L "https://docs.google.com/spreadsheets/d/1ybiI1WgyRs67kGklUXeroi58KDab2KsU_hKutBORiW0/export?gid=1982108250&format=csv"
+
+curl -o ..\src_data\ammo_values.csv -L "https://docs.google.com/spreadsheets/d/1x6LvzrqA9LWXPbzPZBDG8aL4N3Xc_ZxtEFMWpUxQj5c/export?gid=1491084983&format=csv"
+
+curl -o ..\src_data\motion_values.csv -L "https://docs.google.com/spreadsheets/d/1j4bpTbsnp5Xsgw9TP2xv6d8R4qk0ErpE9r_5LGIDraU/export?gid=0&format=csv"
+curl -o ..\src_data\swing_values.csv -L "https://docs.google.com/spreadsheets/d/1j4bpTbsnp5Xsgw9TP2xv6d8R4qk0ErpE9r_5LGIDraU/export?gid=506566839&format=csv"
+
+curl -o ..\src_data\bosses.csv -L "https://docs.google.com/spreadsheets/d/1aujq95UfL_oUs3voPt3nGqM1hLhaVJOj6JKB6Np3FD8/export?gid=1466577964&format=csv"
+curl -o ..\src_data\difficulty_scaling.csv -L "https://docs.google.com/spreadsheets/d/1aujq95UfL_oUs3voPt3nGqM1hLhaVJOj6JKB6Np3FD8/export?gid=466524610&format=csv"
